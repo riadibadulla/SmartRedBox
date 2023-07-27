9c0022b3a010a4e10fcb8950647ee9f370553641
nmpn <s.chernbumroong@outlook.com>
2023-07-27 15:41:14 +0100

    merge Lukes

--- src/util.py
+++ src/util.py
@@ -11,7 +11,17 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.mapreduce import MapReduceChain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import TokenTextSplitter
+import os
+from langchain.docstore.document import Document
+import pandas as pd
+import api_keys
 
+#anthropic
+import anthropic
+import PyPDF2
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
+api_key = os.getenv("OPENAI_API_KEY")
 def summarise(path_to_pdf = 'data/a_pro-innovation_approach_to_AI_regulation.pdf'):
     llm = OpenAI(temperature=0, openai_api_key=api_key) 
     # llm = Claude()
@@ -57,5 +67,40 @@ def get_position(submission):
         position = 'We do not have any position on this submission at the moment.'
     return position
 
-    chain = load_summarize_chain(llm, chain_type="map_reduce")
-    return chain.run(docs)
+def doc_to_json(path):
+    system="""
+    You are an AI assistant that converts document text into valid json in the following format:
+
+    {
+        "Type": <type of document, this could be an email, meeting minutes, submission>,
+        "Title": <title of the document>,
+        "Topic": <overall topic discussed in the document>,
+        "Summary": <summary of the document content>,
+        "Content": null,
+        "Relevant People": <list of people mentioned in the document, inlcuding sender and recipients, format as: Forname Surname>,
+        "Director": <director mentioned in the document>,
+        "Team": <teams mentioned in the document that need to give clearance, where a name is provided>,
+        "Sentiment": <sentiment analysis of the document, should be a floating point number between -1.00 and 1.00, with -1.00 being negative and 1.00 being positive and 0.00 being neutral, must be filled>,
+        "Deadline": <deadline of any actions mentioned in the document, format as exactly as shown in the document>,
+        "Actions": <any follow-up actions mentioned in the document, with added deadline as per "Deadline" field>,
+        "Notes": <any notes attached to the document>,
+        "Date": <date of the document in the format: YYYY-MM-DD, if no date is found then null>
+    }
+
+    You must extract this information from the document accuractly.
+    """
+
+    prompt = extract_text(path)
+
+    response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+            {"role": "system", "content": system},
+            {"role": "user", "content": prompt},
+        ]
+    )
+
+    print(response['choices'][0]['message']['content'])
+
+def summary_with_claude(fine_name='regulations.pdf'):
+    print(f'The sentiment is: {sentiment}')
